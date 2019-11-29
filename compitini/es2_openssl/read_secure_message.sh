718351adfa588c259f0f866203485a89c8823dbf
Gabor Galazzo <gabor.galazzo@star-group.net>
2019-11-29 12:07:20 +0100

    compitini

---
+++ compitini/es2_openssl/read_secure_message.sh
@@ -0,0 +1,5 @@
+[ $# -lt 3 ] && { echo "Usage: $0 secret_message_file secret_password_file private_key"; exit 1; }
+
+
+password=$(openssl pkeyutl -decrypt -inkey "$3" -in "$2")
+openssl aes-256-ecb -in "$1" -d -k $password -pbkdf2
